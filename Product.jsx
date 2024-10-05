2cb26e20e4ad34845218584d43c9edf3246e6cd6
React-Crafter <abulbasar1725@gmail.com>
2024-10-05 04:07:44 -0700

    Add files via upload

---
+++ Product.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Product() {
+  return (
+    <div className='c-m'>
+        <h2>Hello! I am from the Products</h2>
+        <p>
+        eum, voluptates dignissimos reprehenderit facere excepturi pariatur earum!<br/>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit sapiente porro quasi <br/>
+        libero mollitia adipisci voluptatum officiis animi corporis nisi amet, <br/>
+        dolore unde quiamolestiae, quidem eligendi non<br/>
+        repellendus earum. Lorem ipsum dolor sit amet conse<br/>
+        molestias sapiente, iusto recusandae<br/>
+        id necessitatibus nostrum maxime illo commodi quidem? Non.</p>
+    </div>
+  )
+}
+
+export default Product
